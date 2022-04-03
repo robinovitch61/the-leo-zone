bc2a77de1357b4789fb20172f3f608932be677cb
robinovitch61 <robinovitch61@users.noreply.github.com>
2022-04-03 20:49:10 +0000

    deploy: 480c3805bc8d2f7c7ed7e658bd82eaf3873f2ff2

--- pixels/index.js
+++ pixels/index.js
@@ -1,31 +1,35 @@
 var stuff = document.getElementById("stuff");
 var grid = document.getElementById("grid");
 var gridSidePixelCount = 20;
-var numPixels = gridSidePixelCount * gridSidePixelCount;
-var pixels = [];
-var pixelBorderStyle = "1px solid black";
-// window.addEventListener("resize", () => {
-//   grid.style.width = `${grid.client}px`;
-//   pixelSidePx = gridHeightPx / gridSidePixelCount;
-// });
+var pixelBorderStyle = "0.5px solid black";
 var gridHeightPx = Math.round(grid.clientHeight);
 grid.style.height = "".concat(gridHeightPx, "px");
 grid.style.width = "".concat(gridHeightPx, "px");
 var pixelSidePx = gridHeightPx / gridSidePixelCount;
-for (var i = 0; i < numPixels; i++) {
+function getPixelForPosition(down, across) {
     var pixel = document.createElement("div");
     pixel.classList.add("pixel");
     pixel.style.width = "".concat(pixelSidePx, "px");
     pixel.style.height = "".concat(pixelSidePx, "px");
     pixel.style.borderTop = pixelBorderStyle;
     pixel.style.borderLeft = pixelBorderStyle;
-    if ((i + 1) % gridSidePixelCount === 0) {
+    if (across == gridSidePixelCount - 1) {
         pixel.style.borderRight = pixelBorderStyle;
     }
-    if (Math.floor(i / gridSidePixelCount + 1) === gridSidePixelCount) {
+    if (down == gridSidePixelCount - 1) {
         pixel.style.borderBottom = pixelBorderStyle;
     }
-    pixels.push(pixel);
-    grid.appendChild(pixel);
+    return pixel;
+}
+var row = [];
+var pixels = [];
+for (var down = 0; down < gridSidePixelCount; down++) {
+    row = [];
+    for (var across = 0; across < gridSidePixelCount; across++) {
+        var pixel = getPixelForPosition(down, across);
+        row.push(pixel);
+        grid.appendChild(pixel);
+    }
+    pixels.push(row);
 }
 grid.style.setProperty("--repeatNumber", "".concat(gridSidePixelCount));
