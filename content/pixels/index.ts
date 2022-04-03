480c3805bc8d2f7c7ed7e658bd82eaf3873f2ff2
Leo Robinovitch <leorobinovitch@gmail.com>
2022-04-03 16:48:47 -0400

    2d grid of pixels

--- content/pixels/index.ts
+++ content/pixels/index.ts
@@ -2,34 +2,42 @@ const stuff = document.getElementById("stuff");
 const grid = document.getElementById("grid");
 
 const gridSidePixelCount = 20;
-const numPixels = gridSidePixelCount * gridSidePixelCount;
-const pixels = [];
-const pixelBorderStyle = "1px solid black";
-
-// window.addEventListener("resize", () => {
-//   grid.style.width = `${grid.client}px`;
-//   pixelSidePx = gridHeightPx / gridSidePixelCount;
-// });
+const pixelBorderStyle = "0.5px solid black";
 
 const gridHeightPx = Math.round(grid.clientHeight);
 grid.style.height = `${gridHeightPx}px`;
 grid.style.width = `${gridHeightPx}px`;
 const pixelSidePx = gridHeightPx / gridSidePixelCount;
 
-for (let i = 0; i < numPixels; i++) {
+function getPixelForPosition(down: number, across: number) {
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
   pixel.style.width = `${pixelSidePx}px`;
   pixel.style.height = `${pixelSidePx}px`;
   pixel.style.borderTop = pixelBorderStyle;
   pixel.style.borderLeft = pixelBorderStyle;
-  if ((i + 1) % gridSidePixelCount === 0) {
+
+  if (across == gridSidePixelCount - 1) {
     pixel.style.borderRight = pixelBorderStyle;
   }
-  if (Math.floor(i / gridSidePixelCount + 1) === gridSidePixelCount) {
+  if (down == gridSidePixelCount - 1) {
     pixel.style.borderBottom = pixelBorderStyle;
   }
-  pixels.push(pixel);
-  grid.appendChild(pixel);
+  return pixel;
 }
+
+type Pixel = HTMLDivElement;
+
+let row: Pixel[] = [];
+const pixels: Pixel[][] = [];
+for (let down = 0; down < gridSidePixelCount; down++) {
+  row = [];
+  for (let across = 0; across < gridSidePixelCount; across++) {
+    const pixel = getPixelForPosition(down, across);
+    row.push(pixel);
+    grid.appendChild(pixel);
+  }
+  pixels.push(row);
+}
+
 grid.style.setProperty("--repeatNumber", `${gridSidePixelCount}`);
